7fff69f704f1359fbb5df5e63b77bb2a30dc5e83
MasaGratoR <masagrator@gmail.com>
2026-05-21 00:13:46 +0200

    Add files via upload

--- source/smd_parser.cpp
+++ source/smd_parser.cpp
@@ -21,7 +21,27 @@ extern "C" {
 #include <unordered_map>
 #include <unordered_set>
 #include <vector>
+#ifndef __SWITCH__
+unsigned int crc32Calculate(const unsigned char *message, size_t len) {
+   int i, j;
+   unsigned int byte, crc, mask;
+
+   i = 0;
+   crc = 0xFFFFFFFF;
+   while (i < len) {
+      byte = message[i];            // Get next byte.
+      crc = crc ^ byte;
+      for (j = 7; j >= 0; j--) {    // Do eight times.
+         mask = -(crc & 1);
+         crc = (crc >> 1) ^ (0xEDB88320 & mask);
+      }
+      i = i + 1;
+   }
+   return ~crc;
+}
+#else
 #include <switch.h>
+#endif
 
 // Force everything in this block to be optimized for speed (O3)
 #pragma GCC push_options
